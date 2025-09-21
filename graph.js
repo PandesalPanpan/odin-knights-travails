0c611d0aabaff7bb4cb0bebd423abd245166efa6
PandesalPanpan <petermarticio@gmail.com>
2025-09-22 06:48:26 +0800

    Feat: Initialize the graph matrix

---
+++ graph.js
@@ -0,0 +1,29 @@
+import prettyPrint from "./prettyprint.js";
+// Create a chessboard?
+/* 
+    8x8, I need to create a graph with adjacent list, in each position
+    include the possible movies
+
+*/
+// Print a chessboard with their two dimensional positions
+// [0,0],[0,1]
+
+export default class Graph {
+    board;
+
+    initialize = () => {
+        // Two nested loops?
+        const nestedArray = [];
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                nestedArray.push([i, j]);
+            }
+        }
+        this.board = nestedArray;
+    }
+
+}
+
+const graph = new Graph();
+graph.initialize();
+prettyPrint(graph.board);
