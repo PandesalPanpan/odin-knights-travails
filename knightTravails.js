ed0459c6cea2084e57673cf0e1bc27794f8a8fa1
PandesalPanpan <petermarticio@gmail.com>
2025-09-22 08:18:51 +0800

    Feat: Add getKnightMoves that gets 8 legal moves from current position

--- knightTravails.js
+++ knightTravails.js
@@ -23,10 +23,46 @@ export default class KnightTravails {
     }
 
     // Identify the possible moves in a current position
-    
+    // Returns an array of possible cells a knigtht can move
+    getKnightMoves = (currentPosition) => {
+        // Two Steps Forward, One Step to Sides
+        const differences = [
+            [1,-2], [2,-1], [1,2], [2,1],
+            [-1,-2], [-2,-1], [-1,2], [-2,1],
+        ]
+        // If the current position is [3,3]
+        /* 
+            [3,3]
+            +1 -2
+            +1 +2
+            +2 -1
+            +2 +1
+
+            -1 -2
+            -1 +2
+            -2 -1
+            -2 +2
+        */
+        // Max moves is always 8
+        // Each possible position must be
+        // One has to have two difference
+        // The other has to have only one difference
+        const moves = differences.map((difference) => {
+            const row = currentPosition[0] + difference[0]
+            const column = currentPosition[1] + difference[1]
+            return [row, column];
+        })
+
+        // Filter out moves that aren't valid.
+
+        return moves
+
+        // Don't add if it outside the valid range of indexes
+    }
 
 }
 
 const graph = new KnightTravails();
 graph.initialize();
-prettyPrint(graph.board);
+/* prettyPrint(graph.board); */
+graph.getKnightMoves([3,3]);
